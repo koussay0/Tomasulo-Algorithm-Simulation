a333e2374e11afeab441114f99a1ca3af07a64a1
koussay0 <49121836+koussay0@users.noreply.github.com>
2025-05-23 15:19:00 +0300

    Update TomasuloSimulator.h

--- ModifiedProjectLast/TomasuloSimulator.h
+++ ModifiedProjectLast/TomasuloSimulator.h
@@ -4,16 +4,17 @@
 #include <unordered_map>
 #include "Instruction.h"
 #include "Memory.h"
+using namespace std;
 
 struct RegisterFile {
-    std::vector<int16_t> regs = std::vector<int16_t>(8, 0);
+    vector<int16_t> regs = std::vector<int16_t>(8, 0);
     int16_t read(int idx) const { return idx == 0 ? 0 : regs[idx]; }
     void write(int idx, int16_t val) { if (idx != 0) regs[idx] = val; }
 };
 
 struct ReservationStation {
-    std::string name;
-    std::string op;
+    string name;
+    string op;
     int busy = 0;
     int Vj = 0, Vk = 0;
     int Qj = -1, Qk = -1;
@@ -27,7 +28,7 @@ struct ReservationStation {
 };
 
 struct FunctionalUnit {
-    std::string type;
+    string type;
     int i1 = 0, i2 = 0, offset = 0;
     FunctionalUnit(const std::string& t, int a=0, int b=0, int c=0) : type(t), i1(a), i2(b), offset(c) {}
     int16_t Operation(const std::string& op) {
@@ -52,14 +53,14 @@ private:
     int completedInstructions = 0;
     int branchCount = 0;
     int mispredictedBranches = 0;
-    std::vector<Instruction> instructions;
+    vector<Instruction> instructions;
     RegisterFile registerFile;
-    std::vector<ReservationStation> reservationStations;
-    std::vector<FunctionalUnit> functionalUnits;
+    vector<ReservationStation> reservationStations;
+    vector<FunctionalUnit> functionalUnits;
     Memory memory;
     void Issue();
     void Execute();
     void Write();
     int getExecutionCycles(const std::string& op) const;
-    std::vector<Instruction> readInstructions(const std::string& filename);
+    vector<Instruction> readInstructions(const std::string& filename);
 };
